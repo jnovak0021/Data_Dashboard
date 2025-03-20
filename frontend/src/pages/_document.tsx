afd58a75e995165747fed8f7f54524eec956cdf9
Matthew <mswift0023@pointloma.edu>
2025-03-20 09:57:45 -0700

    ability to rearrange panes and delete them

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import HomePage from './HomePage'
+//import ThreatIntelComponent from '@/components/ThreatIntelComponent'
+//import SIEMApplication from '@/components/SIEMApplication'
 
 export default function Document() {
   return (
