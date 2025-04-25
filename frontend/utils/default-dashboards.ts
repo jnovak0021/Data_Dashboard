44675dd5fdea56071fff485a113b619fad9378e3
Matthew <mswift0023@pointloma.edu>
2025-04-24 22:52:14 -0700

    Default data outline

---
+++ frontend/utils/default-dashboards.ts
@@ -0,0 +1,29 @@
+import { DashboardType, APIData } from './types';
+
+export const FinanceDashboard: DashboardType = {
+    id: 0,
+    name: "Finance",
+    apis: [
+        {   apiId: 12323,
+            userId: 123,
+            apiName: "sdas",
+            apiString: "sdas",
+            apiKey: "aasd",
+            graphType: "dss",
+            paneX: 123,
+            paneY: 123,
+            parameters: null
+        }
+    ]
+};
+
+// apiId: number;
+// userId: number;
+// apiName: string;
+// apiString: string;
+// apiKey: string;
+// graphType: string;
+// paneX: number;
+// paneY: number;
+// parameters: (string | { parameter: string })[] | null;
+// rootKey?: string;
